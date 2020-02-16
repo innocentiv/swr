893ea968d33371a48803eb3f8bb818dd116e3764
Valerio Innocenti Sedili <info@vinnocenti.com>
2020-02-17 00:25:14 +0100

    build: for reset-pages PR

---
+++ dist/use-swr-pages.d.ts
@@ -0,0 +1,11 @@
+import {
+  pageComponentType,
+  pageOffsetMapperType,
+  pagesResponseInterface
+} from './types'
+export declare function useSWRPages<OffsetType = any, Data = any, Error = any>(
+  pageKey: string,
+  pageFn: pageComponentType<OffsetType, Data, Error>,
+  SWRToOffset: pageOffsetMapperType<OffsetType, Data, Error>,
+  deps?: any[]
+): pagesResponseInterface
